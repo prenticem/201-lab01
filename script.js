79c459392799bbe5bea837afd19821682f5952dd
Peanut <123972373+prenticem@users.noreply.github.com>
2023-03-22 14:20:47 -0500

    lab01 update

---
+++ script.js
@@ -0,0 +1,15 @@
+function questions() {
+    let a1 = prompt("What is your favorite sports team");
+    alert("The " + a1 + " is a good sports team!");
+    let a2 = prompt("What year was the last chapmionship for the " + a1);
+    alert(a2 + " Was a great year for that team");
+    let a3 = prompt("What is your favorite player on the team");
+    alert(a3 + " was the MVP");
+    let a4 = prompt("What is your team colors");
+    alert(a4 + " your teams colors are my favorite color");
+    let a5 = prompt("what is the coach name?");
+    alert(a5 + "That's one of the best coaches ever")
+    let a6 = prompt("Who is the owner?");
+    alert(a6 + "He really care about the team")
+
+}
